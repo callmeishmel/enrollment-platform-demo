1a1cbce2904d930aff85e18dc5614e775b8219d9
Ismael Guajardo <ish.gdo@gmail.com>
2025-04-11 01:13:55 -0600

    Create base schema

---
+++ database/migrations/2025_04_11_062443_create_applications_table.php
@@ -0,0 +1,34 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('applications', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');
+            $table->foreignId('program_id')->constrained()->onDelete('cascade');
+            $table->foreignId('payment_plan_id')->nullable()->constrained()->onDelete('cascade');
+            $table->string('phone');
+            $table->string('city');
+            $table->date('start_date');
+            $table->enum('status', ['submitted', 'under_review', 'approved'])->default('submitted');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('applications');
+    }
+};
