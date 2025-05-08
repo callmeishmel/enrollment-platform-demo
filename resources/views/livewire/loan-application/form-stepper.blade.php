48e7171467492c3c252c2bb75b18fc010bf13a67
Ismael Guajardo <ish.gdo@gmail.com>
2025-05-08 13:12:02 -0600

    Remove unnecessary divs

--- resources/views/livewire/loan-application/form-stepper.blade.php
+++ resources/views/livewire/loan-application/form-stepper.blade.php
@@ -9,22 +9,21 @@
         </div>
         <div class="flex flex-col justify-between h-[450px]">
             <form wire:submit="save" class="flex flex-col justify-between h-full">
-                
+
                 @if($currentStep == 1)
-                    <div class="flex-1">@include('partials.forms.loan-application.step-one')</div>
-                    <div class="object-bottom"><button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Select Program</button></div>
+                    @include('partials.forms.loan-application.step-one')
+                    <button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Select Program</button>
                 @elseif($currentStep == 2)
-                    <div class="flex-1">@include('partials.forms.loan-application.step-two')</div>
-                    <div><button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Select Payment Plan</button></div>
+                    @include('partials.forms.loan-application.step-two')
+                    <button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Select Payment Plan</button>
                 @elseif($currentStep == 3)
-                    <div class="flex-1">@include('partials.forms.loan-application.step-three')</div>
-                    <div><button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Account</button></div>
+                    @include('partials.forms.loan-application.step-three')
+                    <button type="button" wire:click="goToNextStep" class="form-btn min-w-full" >Next: Account</button>
                 @elseif($currentStep == 4)
-                    <div class="flex-1">@include('partials.forms.loan-application.step-four')</div>
-                    <div><button type="submit" class="form-btn min-w-full" >Submit</button></div>
+                    @include('partials.forms.loan-application.step-four')
+                    <button type="submit" class="form-btn min-w-full" >Submit</button>
                 @endif
-                    
-                
+
             </form>
         </div>
     </div>
