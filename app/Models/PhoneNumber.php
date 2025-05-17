d6f37fcabf1ded05a71a2d292a45060c3e242c26
Ismael Guajardo <ish.gdo@gmail.com>
2025-05-17 17:23:40 -0600

    Add set/get phone number formatting

--- app/Models/PhoneNumber.php
+++ app/Models/PhoneNumber.php
@@ -3,6 +3,7 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Casts\Attribute;
 
 class PhoneNumber extends Model
 {
@@ -11,9 +12,13 @@ class PhoneNumber extends Model
         'phone'
     ];
 
-    protected $casts = [
-        'phone' => 'string',
-    ];
+    protected function phone(): Attribute
+    {
+        return Attribute::make(
+            set: fn ($value) => preg_replace('/\D/', '', $value),
+            get: fn ($value) => preg_replace('/(\d{3})(\d{3})(\d{4})/', '($1) $2-$3', $value)
+        );
+    }
 
     public function user()
     {
