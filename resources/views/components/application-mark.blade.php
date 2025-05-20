bfff0b66e9cfe81d087098dcbabf9111dc49f998
Ismael Guajardo <ish.gdo@gmail.com>
2025-05-20 03:06:53 -0600

    Fix logo destination when logged in

--- resources/views/components/application-mark.blade.php
+++ resources/views/components/application-mark.blade.php
@@ -1,4 +1,4 @@
-<a href="{{ route('welcome') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
+<a href="{{ route('dashboard') }}" class="flex items-center space-x-3 rtl:space-x-reverse">
     <img src="/images/logo.png" class="h-8" alt="{{ env('APP_NAME') }} Logo" />
     <span class="self-center text-2xl font-semibold whitespace-nowrap dark:text-white">{{ env('APP_NAME') }}</span>
 </a>
